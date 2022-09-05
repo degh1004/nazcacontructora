c645304956cc5fecd8ba0d9a51bf5ea1a7f98df5
degh1004 <david@se360.co>
2022-09-05 15:04:58 -0500

    add wp button

--- realestate/js/script.js
+++ realestate/js/script.js
@@ -368,20 +368,6 @@ jQuery(function ($) {
     });
 
 
-// Go to Top
-    $(function(){
-        // Scroll Event
-        $(window).on('scroll', function(){
-            var scrolled = $(window).scrollTop();
-            if (scrolled > 600) $('.go-top').addClass('active');
-            if (scrolled < 600) $('.go-top').removeClass('active');
-        });
-        // Click Event
-        $('.go-top').on('click', function() {
-            $("html, body").animate({ scrollTop: "0" },  500);
-        });
-    });
-
 
 
     /*====================================
